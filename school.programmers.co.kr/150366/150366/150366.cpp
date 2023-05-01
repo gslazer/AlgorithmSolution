54428b7d2485990809047b5aa990dd06ae50476c
Gslazer <gslazer2@gmail.com>
2023-05-01 15:09:38 +0900

    programmers solutions

---
+++ school.programmers.co.kr/150366/150366/150366.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+
+using namespace std;
+
+vector<string> parseString(const string& input) {
+    vector<std::string> result;
+    /*stringstream ss(input);
+    string token;
+    while (std::getline(ss, token, ' ')) {
+        result.push_back(token);
+    }*/
+    return result;
+}
+
+vector<string> solution(vector<string> commands) {
+
+    vector<string*>temp;
+    for (const string& command : commands)
+    {
+        const auto& parsedString = parseString(command);
+    }
+
+    vector<string> answer ;
+
+    return answer;
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
